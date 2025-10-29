ba0fbc63cefbfda1a14d72455ea013652e426202
Bryan Nguyen <125615689+learust@users.noreply.github.com>
2025-10-29 12:07:19 -0400

    incorrect imports
    removed review.sentiment from sentiment

--- review-service/app.py
+++ review-service/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from review.sentiment import polarity_scores, stars
+from sentiment import polarity_scores, stars
 
 app = Flask(__name__)
 CORS(app)
